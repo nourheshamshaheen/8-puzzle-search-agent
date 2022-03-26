84f3112448120f9fc3b534cb2dde92b2ad256de8
Nour Hesham <nourheshamshaheen11@gmail.com>
2022-03-26 21:12:49 +0200

    Change logic and remove State class

--- Game.py
+++ Game.py
@@ -1,8 +1,10 @@
 from PySide2.QtWidgets import QLabel
 from PySide2.QtWidgets import QPushButton, QRadioButton, QLineEdit
 from Puzzle import Puzzle
+from datetime import datetime as time
 
-class Game:
+
+class Game:  # GUI Driver class
 
     def __init__(self, ui):
         self.ui = ui
@@ -10,6 +12,7 @@ class Game:
         self.radioDFS = self.ui.findChild(QRadioButton, 'radioDFS')
         self.radioASTARMAN = self.ui.findChild(QRadioButton, 'radioASTARMAN')
         self.radioASTAREUC = self.ui.findChild(QRadioButton, 'radioASTAREUC')
+
         self.state_index = 0
         self.states_path = []
 
@@ -31,7 +34,9 @@ class Game:
         self.label7 = self.ui.findChild(QLabel, 'label7')
         self.label8 = self.ui.findChild(QLabel, 'label8')
 
+        self.label0.setText(" ")
         self.lineEdit = self.ui.findChild(QLineEdit, 'lineEdit')
+        self.lineEdit.setText("125340678")
 
         self.buttonRun.clicked.connect(self.run)
         self.buttonNext.clicked.connect(self.next)
@@ -39,56 +44,94 @@ class Game:
         self.buttonSkipNext.clicked.connect(self.skip_next)
         self.buttonSkipPrev.clicked.connect(self.skip_prev)
 
-
-
-    def get_path(self, state):
-        path = []
-        path.append(state)
-        while state.parent is not None:
-            state = state.parent
+    def get_path(self, final_state, parent_map):  # getting path from parent map and goal state
+        path = [final_state]
+        state = parent_map[final_state]
+        while state is not None:
             path.append(state)
+            state = parent_map[state]
         return path
 
-    def print_state(self, state):
-        self.label0.setText(str(state.array[0]))
-        self.label1.setText(str(state.array[1]))
-        self.label2.setText(str(state.array[2]))
-        self.label3.setText(str(state.array[3]))
-        self.label4.setText(str(state.array[4]))
-        self.label5.setText(str(state.array[5]))
-        self.label6.setText(str(state.array[6]))
-        self.label7.setText(str(state.array[7]))
-        self.label8.setText(str(state.array[8]))
+    def print_state(self, final_state):  # utility function to display states on GUI board
+        state = ""
+        for character in final_state:
+            if character != '0':
+                state += character
+            else:
+                state += ' '
 
-    def run(self):
-        initial_values = []
+        self.label0.setText(str(state[0]))
+        self.label1.setText(str(state[1]))
+        self.label2.setText(str(state[2]))
+        self.label3.setText(str(state[3]))
+        self.label4.setText(str(state[4]))
+        self.label5.setText(str(state[5]))
+        self.label6.setText(str(state[6]))
+        self.label7.setText(str(state[7]))
+        self.label8.setText(str(state[8]))
+
+    def get_inversions(self, array):  # function to get number of inversions in certain state
+        inv_count = 0
+        empty_value = 0
+        for i in range(0, 9):
+            for j in range(i + 1, 9):
+                if array[j] != empty_value and array[i] != empty_value and array[i] > array[j]:
+                    inv_count += 1
+        return inv_count
+
+    def isSolvable(self, puzzle):
+        inv_count = self.get_inversions([j for sub in puzzle for j in sub])
+        return (inv_count % 2 == 0)
+
+    def run(self):  # main function running the chosen algorithm
         initial_string = self.lineEdit.text()
-        initial_string = initial_string.split()
-        for element in initial_string:
-            initial_values.append(int(element))
-        puzzle = Puzzle(initial_values)
+        arr = [[int(initial_string[0]), int(initial_string[1]), int(initial_string[2])],
+               [int(initial_string[3]), int(initial_string[4]), int(initial_string[5])],
+               [int(initial_string[6]), int(initial_string[7]), int(initial_string[8])]]
+        print("Solvability check using function: " + str(self.isSolvable(arr)))
+        if not self.isSolvable(arr):
+            return
+        puzzle = Puzzle(initial_string)
         if self.radioBFS.isChecked():
-            self.goal_state = puzzle.BFS()
+            start = time.now()
+            goal_state, parent_map, cost_map = puzzle.BFS()
+            end = time.now()
+            print("BFS running time (in millis) = " + str((end - start).microseconds / 1000))
         elif self.radioDFS.isChecked():
-            self.goal_state = puzzle.DFS()
+            start = time.now()
+            goal_state, parent_map, cost_map = puzzle.DFS()
+            end = time.now()
+            print("DFS running time (in millis) = " + str((end - start).microseconds / 1000))
         elif self.radioASTARMAN.isChecked():
-            self.goal_state = puzzle.A_star(1)
+            start = time.now()
+            goal_state, parent_map, cost_map = puzzle.A_star_man()
+            end = time.now()
+            print("A* man running time (in millis) = " + str((end - start).microseconds / 1000))
         elif self.radioASTAREUC.isChecked():
-            self.goal_state = puzzle.A_star(2)
-        self.display(self.goal_state)
+            start = time.now()
+            goal_state, parent_map, cost_map = puzzle.A_star_euc()
+            end = time.now()
+            print("A* euc running time (in millis) = " + str((end - start).microseconds / 1000))
+        if puzzle.solvable is True:
+            self.display(goal_state, parent_map)
+            print("SEARCH DEPTH IS: ", max(cost_map.values()))
+        else:
+            print("UNSOLVABLE")
 
-    def display(self, state):
-        self.states_path = self.get_path(state)
+    def display(self, state, parent_map):
+        self.states_path = self.get_path(state, parent_map)
         self.state_index = len(self.states_path) - 1
         self.print_state(self.states_path[self.state_index])
         string = '1/' + str(len(self.states_path))
         self.pathNodes.setText(string)
 
+    # functions to define what arrows do:
+
     def prev(self):
         if len(self.states_path) > self.state_index + 1:
             self.state_index = self.state_index + 1
             self.print_state(self.states_path[self.state_index])
-            string = str(len(self.states_path)-self.state_index) +'/' + str(len(self.states_path))
+            string = str(len(self.states_path) - self.state_index) + '/' + str(len(self.states_path))
             self.pathNodes.setText(string)
         return
 
@@ -96,9 +139,10 @@ class Game:
         if self.state_index > 0:
             self.state_index = self.state_index - 1
             self.print_state(self.states_path[self.state_index])
-            string = str(len(self.states_path)-self.state_index) + '/' + str(len(self.states_path))
+            string = str(len(self.states_path) - self.state_index) + '/' + str(len(self.states_path))
             self.pathNodes.setText(string)
         return
+
     def skip_next(self):
         self.state_index = 0
         self.print_state(self.states_path[0])
--- Puzzle.py
+++ Puzzle.py
@@ -1,93 +1,245 @@
+import math
 from collections import deque
 import heapq
-from State import State
 
 class Puzzle:
-    def __init__(self, initial_values):
-        self.goal_state = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-        self.initial_state = State(None, initial_values, 0)
-        self.initial_state_man = State(None, initial_values, 1)
-        self.initial_state_euc = State(None, initial_values, 2)
+    def __init__(self, initial_state):
+        self.goal_state = "012345678"
+        self.initial_state = initial_state
+        self.solvable = True
 
     #compare state w/ goal state to test for success
     def goal_test(self, state):
-        if state.array == self.goal_state:
+        if state == self.goal_state:
             return True
         return False
 
+    # How to get children? Try every swap between the '0' element and the element next to it
+    def get_children(self, state):
+        children = []
+        index = state.find('0')  # find zero index to determine swaps with other elements
+        if index not in [0, 3, 6]:
+            child = self.swap_left(state, index)
+            children.append(child)
+        if index not in [2, 5, 8]:
+            child = self.swap_right(state, index)
+            children.append(child)
+        if index not in [0, 1, 2]:
+            child = self.swap_up(state, index)
+            children.append(child)
+        if index not in [6, 7, 8]:
+            child = self.swap_down(state, index)
+            children.append(child)
+        return children
+
     def BFS(self):
+        expanded_nodes = 0
         frontier = deque()  # frontier is a FIFO queue: append: enqueue, popleft: dequeue
         frontier.append(self.initial_state)
-        explored = []
+        explored = set()
+        parent_map = {self.initial_state: None}
+        cost_map = {self.initial_state: 0}
 
-        while frontier: # while frontier queue is not empty (yields true)
+        while frontier:  # while frontier queue is not empty (yields true)
             print(len(frontier))
             state = frontier.popleft()
-            explored.append(state.array)
+            expanded_nodes += 1
+            explored.add(state)
 
             if self.goal_test(state):
-                return state
+                self.solvable = True
+                print("BFS expanded nodes: " + str(expanded_nodes))
+                return state, parent_map, cost_map
 
-            children = state.get_children()
+            children = self.get_children(state)
             for child in children:
-                if not self.exists(child, frontier) and child.array not in explored:
+                if child not in frontier and child not in explored:
                     frontier.append(child)
-        return None
+                    parent_map[child] = state
+                    cost_map[child] = cost_map[state] + 1
+        self.solvable = False
+        print("BFS expanded nodes: " + str(expanded_nodes))
+        return None, parent_map, cost_map
 
     def DFS(self):
-        frontier = deque()  # frontier is a LIFO stack: append: PUSH, pop: POP
+        expanded_nodes = 0
+        frontier = deque()  # frontier is a LIFO stack: append: push, pop: pop
         frontier.append(self.initial_state)
-        explored = []
+        explored = set()
+        parent_map = {self.initial_state: None}
+        cost_map = {self.initial_state: 0}
 
-        while frontier: # while frontier stack is not empty (yields true)
+        while frontier:  # while frontier queue is not empty (yields true)
+            print(len(frontier))
             state = frontier.pop()
-            explored.append(state.array)
+            expanded_nodes += 1
+            explored.add(state)
 
             if self.goal_test(state):
-                return state
+                self.solvable = True
+                print("DFS expanded nodes: " + str(expanded_nodes))
+                return state, parent_map, cost_map
 
-            children = state.get_children()
+            children = self.get_children(state)
             for child in children:
-                if child not in frontier and child.array not in explored:
+                if child not in frontier and child not in explored:
                     frontier.append(child)
-        return None
+                    parent_map[child] = state
+                    cost_map[child] = cost_map[state] + 1
 
-    def A_star(self, type): # type is 1 for manhattan and 2 for euclidean
+        self.solvable = False
+        print("DFS expanded nodes: " + str(expanded_nodes))
+        return None, parent_map, cost_map
 
-        if type == 1:
-            frontier = [self.initial_state_man] # frontier is a priority queue/minheap
-        if type == 2:
-            frontier = [self.initial_state_euc]
+    def A_star_man(self):
+        expanded_nodes = 0
+        explored = set()
+        parent_map = {self.initial_state: None}
+        cost_map = {self.initial_state: 0}
+        heuristic_map = {self.initial_state: self.get_man_h(self.initial_state)}
+        f_map = {self.initial_state: self.get_man_h(self.initial_state)}
+        frontier = []  # frontier is a priority queue/minheap
+        heapq.heappush(frontier, (f_map[self.initial_state], self.initial_state))
 
-        heapq.heapify(frontier)
-        explored = []
-
-        while frontier: # while frontier priority queue is not empty (yields true)
-            state = heapq.heappop(frontier)
-            explored.append(state.array)
+        while frontier:  # while frontier priority queue is not empty (yields true)
+            current_cost, state = heapq.heappop(frontier)
+            print(str(len(frontier)))
+            expanded_nodes += 1
+            explored.add(state)
 
             if self.goal_test(state):
-                return state
+                self.solvable = True
+                print("A* (manhattan) expanded nodes: " + str(expanded_nodes))
+                return state, parent_map, cost_map
 
-            children = state.get_children()
+            children = self.get_children(state)
             for child in children:
-                if not self.exists(child, frontier) and child.array not in explored:
-                    heapq.heappush(frontier, child)
-                elif self.exists(child, frontier):
-                    index = self.get_index(child, frontier)
-                    if frontier[index] > child:
-                        frontier[index] = child
+                if child not in frontier and child not in explored:
+                    parent_map[child] = state
+                    cost_map[child] = cost_map[state] + 1
+                    heuristic_map[child] = self.get_man_h(child)
+                    f_map[child] = cost_map[child] + heuristic_map[child]
+                    heapq.heappush(frontier, (f_map[child], child))
+                elif child in frontier:
+                    if cost_map[state] + 1 < cost_map[child]:
+                        frontier.remove((f_map[child], child))
+                        parent_map[child] = state
+                        cost_map[child] = cost_map[state] + 1
+                        heuristic_map[child] = self.get_man_h(child)
+                        f_map[child] = cost_map[child] + heuristic_map[child]
+                        heapq.heappush(frontier, (f_map[child], child))
                         heapq.heapify(frontier)
-        return None
+        self.solvable = False
+        print("A* (manhattan) expanded nodes: " + str(expanded_nodes))
+        return None, parent_map, cost_map
 
-    def exists(self, element, mylist):
-        for i in mylist:
-            if element.array == i.array:
-                return True
-        return False
+    def A_star_euc(self):
+        explored = set()
+        expanded_nodes = 0
+        parent_map = {self.initial_state: None}
+        cost_map = {self.initial_state: 0}
+        heuristic_map = {self.initial_state: self.get_euc_h(self.initial_state)}
+        f_map = {self.initial_state: self.get_euc_h(self.initial_state)}
 
-    def get_index(self, element, mylist):
-        for i in mylist:
-            if element.array == i.array:
-                return mylist.index(i)
-        return None
+        frontier = []  # frontier is a priority queue/minheap
+        heapq.heappush(frontier, (f_map[self.initial_state], self.initial_state))
+
+        while frontier:  # while frontier priority queue is not empty (yields true)
+            current_cost, state = heapq.heappop(frontier)
+            print(str(len(frontier)))
+            explored.add(state)
+            expanded_nodes += 1
+
+            if self.goal_test(state):
+                self.solvable = True
+                print("A* (euclidean) expanded nodes: " + str(expanded_nodes))
+                return state, parent_map, cost_map
+
+            children = self.get_children(state)
+            for child in children:
+                if child not in frontier and child not in explored:
+                    parent_map[child] = state
+                    cost_map[child] = cost_map[state] + 1
+                    heuristic_map[child] = self.get_euc_h(child)
+                    f_map[child] = cost_map[child] + heuristic_map[child]
+                    heapq.heappush(frontier, (f_map[child], child))
+                elif child in frontier:
+                    if cost_map[state] + 1 < cost_map[child]:
+                        parent_map[child] = state
+                        cost_map[child] = cost_map[state] + 1
+                        heuristic_map[child] = self.get_man_h(child)
+                        f_map[child] = cost_map[child] + heuristic_map[child]
+                        heapq.heappush(frontier, (f_map[child], child))
+        self.solvable = False
+        print("A* (euclidean) expanded nodes: " + str(expanded_nodes))
+        return None, parent_map, cost_map
+
+    # functions to swap 0 for elements around it:
+
+    def swap_right(self, state, index):
+        new_state = ""
+        for character in range(0, len(state)):
+            if len(new_state) == (index+1):
+                new_state += state[index]
+            elif len(new_state) == index:
+                new_state += state[index+1]
+            else:
+                new_state += state[character]
+        return new_state
+
+    def swap_left(self, state, index):
+        new_state = ""
+        for character in range(0, len(state)):
+            if len(new_state) == (index - 1):
+                new_state += state[index]
+            elif len(new_state) == index:
+                new_state += state[index - 1]
+            else:
+                new_state += state[character]
+        return new_state
+
+    def swap_up(self, state, index):
+        new_state = ""
+        for character in range(0, len(state)):
+            if len(new_state) == (index - 3):
+                new_state += state[index]
+            elif len(new_state) == index:
+                new_state += state[index - 3]
+            else:
+                new_state += state[character]
+        return new_state
+
+    def swap_down(self, state, index):
+        new_state = ""
+        for character in range(0, len(state)):
+            if len(new_state) == (index + 3):
+                new_state += state[index]
+            elif len(new_state) == index:
+                new_state += state[index + 3]
+            else:
+                new_state += state[character]
+        return new_state
+
+    # functions to calculate heruistic for certain state:
+
+    def get_man_h(self, state):
+        # goal_state = "012345678"
+        initial_config = state
+        man_distance = 0
+        for i, item in enumerate(initial_config):
+            if not int(i) == 0:
+                prev_row, prev_col = int(i / 3), i % 3
+                goal_row, goal_col = int(int(item) / 3), int(item) % 3
+                man_distance += abs(prev_row - goal_row) + abs(prev_col - goal_col)
+        return man_distance
+
+    def get_euc_h(self, state):
+        #goal_state = "012345678"
+        initial_config = state
+        euc_distance = 0
+        for i, item in enumerate(initial_config):
+            if not int(i) == 0:
+                current_row, current_col = int(i / 3), i % 3
+                goal_row, goal_col = int(int(item) / 3), int(item) % 3
+                euc_distance += math.sqrt((current_row - goal_row)*(current_row - goal_row) + (current_col - goal_col)*(current_col - goal_col))
+        return euc_distance
